a133baec18b96aad9c729efe361244520cecf891
soilflux <lightningbagel1@gmail.com>
2026-06-11 06:18:22 -0400

    first commit

---
+++ dialogues.js
@@ -0,0 +1,19 @@
+
+const linearDialogue = {
+    "": {
+        dialogues: [
+            ["",""],
+        ],
+    },
+    "cafe": {
+        dialogues: [
+            ["kaito", "what are cats for? ;o"],
+            ["yuki", "petting c;"],
+            ["kaito","Cats and Cafes would make a good combo ;o"],
+            ["yuki","yes ;3"],
+            ["kaito","what are we doing here? ;o"],
+            ["yuki","petting cats ;o"],
+            ["kaito",""],
+        ],
+    }
+}
---
+++ script.js
@@ -0,0 +1,80 @@
+const config = {
+    use24Hour: false,
+    interval: 15
+};
+
+const state = {
+    currentDialogue: [],
+    currentIndex: 0
+};
+
+const locations = ["cafe"];
+
+locations.forEach(key => {
+    state[key] = {};
+    state[key].currentIndex = 0;
+    const locationBox = document.getElementById("locationBox");
+    const li = document.createElement('li');
+    li.innerHTML = key + "<br>";
+    li.style.color = 'lightgreen';
+    li.onclick = () => updateDialogueSelect(key);
+    locationBox.appendChild(li);
+});
+
+function updateDialogueSelect(clickedLocation) {
+    state.currentLocation = clickedLocation;
+    advance();
+}
+document.addEventListener('keydown', function (event) {
+    if (event.key === ' ' || event.code === 'Space') {
+        event.preventDefault();
+        advance();
+    }
+});
+
+function advance() {
+    const currentIndex = state[state.currentLocation].currentIndex;
+    const currentPrompt = linearDialogue[state.currentLocation].dialogues[currentIndex];
+    const speaker = currentPrompt[0];
+    const text = currentPrompt[1];
+    storyBox.setAttribute("data-character", speaker);
+    document.getElementById("storyBox").innerHTML = text;
+
+    state[state.currentLocation].currentIndex += 1;
+}
+
+function nextInvestigateDialogue(el, location) {
+    const currentIndex = location.currentIndex;
+    if (location.dialogues.length < currentIndex + 1) return;
+    const dialogue = location.dialogues[currentIndex]
+        .map(([name, text]) => `<span class="${name.toLowerCase()}">${text}</span>`);
+    focusDialogue(dialogue);
+    location.currentIndex = currentIndex + 1;
+    if (location.dialogues.length < currentIndex + 2) el.style.color = 'lightgray';
+}
+
+function focusDialogue(dialogue) {
+    state.currentDialogue = dialogue;
+    document.addEventListener('click', advanceDialogue, true);
+    document.getElementById('storyBox').innerHTML = "";
+
+    const fakeClick = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+
+    advanceDialogue(fakeClick);
+}
+
+function advanceDialogue(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    document.getElementById('storyBox').innerHTML += state.currentDialogue[state.currentIndex] + '<br>';
+    state.currentIndex += 1;
+    if (state.currentIndex >= state.currentDialogue.length) {
+        document.getElementById('storyBox').innerHTML += " ->";
+        document.removeEventListener('click', advanceDialogue, true);
+        state.currentIndex = 0;
+    }
+}
